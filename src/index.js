6642d1badea4a53c45368a2a769217447e042d37
abhi00 <abhijit.pandit808@gmail.com>
2021-09-13 21:11:46 +0100

    added CORS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ var myindoorgarden = require('./routes/myindoorgarden.js');
 
 app.use('/myindoorgarden', myindoorgarden);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Backend running at http://localhost:5000.`)
 })
