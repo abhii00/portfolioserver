86e22e3d110fc63d66db3b357ff56b1b4c13c9f8
abhi00 <abhijit.pandit808@gmail.com>
2021-09-13 21:14:53 +0100

    used express cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 var myindoorgarden = require('./routes/myindoorgarden.js');
 
 app.use('/myindoorgarden', myindoorgarden);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.listen(process.env.PORT, () => {
   console.log(`Backend running at http://localhost:5000.`)
