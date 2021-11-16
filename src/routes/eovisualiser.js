538fd79c62182fb44a7d6927a139383be6489d96
abhi00 <abhijit.pandit808@gmail.com>
2021-11-16 00:47:33 +0000

    changes to eovisualiser response

--- src/routes/eovisualiser.js
+++ src/routes/eovisualiser.js
@@ -7,7 +7,7 @@ router
   .get('/satellite-tle', function(req, res){
       fetch('https://celestrak.com/NORAD/elements/gp.php?GROUP=ACTIVE&FORMAT=TLE')
       .then(r => r.text())
-      .then(text => res.text(text))
+      .then(text => res.send(text))
   })
 
 module.exports = router;
