fb6aa45f6a3a399c5ba67a11f41371093d1ac00d
abhi00 <abhijit.pandit808@gmail.com>
2022-05-21 17:11:36 +0100

    todos

--- src/routes/eovisualiser.js
+++ src/routes/eovisualiser.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const fetch = require('node-fetch');
 
+//TODO set sources for EO data
+
 router
   .get('/satellite-tle', function(req, res){
       fetch('https://celestrak.com/NORAD/elements/gp.php?GROUP=ACTIVE&FORMAT=TLE')
--- src/routes/myindoorgarden.js
+++ src/routes/myindoorgarden.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const test_garden = require('../assets/myindoorgarden/test_garden.json')
 
-/* TODO setup mongoDB database to allow for myindoorgarden to save files */
-/* TODO setup mongoDB upload AP for myindoorgarden */
+//TODO setup mongoDB database to allow for myindoorgarden to save files
+//TODO setup mongoDB upload AP for myindoorgarden
 
 router
   .get('/test', function(req, res){
